95eb3cb12cd6b32ce7f0baabeefb0f8e230695d1
Nuwanrg <>
2020-11-19 22:57:28 +0530

    add reset password component

---
+++ src/components/resetPassword.jsx
@@ -0,0 +1,175 @@
+import React, { Component } from "react";
+import Form from "react-validation/build/form";
+import Input from "react-validation/build/input";
+import CheckButton from "react-validation/build/button";
+import { isEmail } from "validator";
+
+import AuthService from "../services/auth.service";
+import Link from "@material-ui/core/Link";
+
+const required = (value) => {
+  if (!value) {
+    return (
+      <div className="alert alert-danger" role="alert">
+        This field is required!
+      </div>
+    );
+  }
+};
+
+const email = (value) => {
+  if (!isEmail(value)) {
+    return (
+      <div className="alert alert-danger" role="alert">
+        This is not a valid email.
+      </div>
+    );
+  }
+};
+
+
+
+class ResetPassword extends Component {
+
+  constructor(props) {
+    super(props);
+    this.resetPassword = this.resetPassword.bind(this);
+    this.onChangeEmail = this.onChangeEmail.bind(this);
+
+    this.state = {
+      email: "",
+      successful: false,
+      message: ""
+    };
+  }
+
+  onChangeEmail(e) {
+    this.setState({
+      email: e.target.value
+    });
+  }
+
+  resetPassword(e) {
+    e.preventDefault();
+
+    this.setState({
+      message: "",
+      successful: false
+    });
+
+    this.form.validateAll();
+
+    if (this.checkBtn.context._errors.length === 0) {
+      AuthService.resetPassword(
+        this.state.email
+      ).then(
+        response => {
+          this.setState({
+            message: response.data.message,
+            successful: true
+          });
+        },
+        error => {
+          const resMessage =
+            (error.response &&
+              error.response.data &&
+              error.response.data.message) ||
+            error.message ||
+            error.toString();
+
+          this.setState({
+            successful: false,
+            message: resMessage
+          });
+        }
+      );
+    }
+  }
+
+
+  render() {
+    return (
+      <div className="col-md-12">
+        <div className="card card-container">
+          <img
+            src="//ssl.gstatic.com/accounts/ui/avatar_2x.png"
+            alt="profile-img"
+            className="profile-img-card"
+          />
+
+          <Form
+            onSubmit={this.resetPassword}
+            ref={c => {
+              this.form = c;
+            }}
+          >
+            {!this.state.successful && (
+              <div>
+            <div className="form-group">
+              <label>Forgot your password?</label>
+              <label>
+                Enter the email that you registered with us and we will send you
+                a link to reset the password.
+              </label>
+            </div>
+            <div className="form-group">
+              <label htmlFor="email">Email</label>
+              <Input
+                type="text"
+                className="form-control"
+                name="email"
+                value={this.state.email}
+                onChange={this.onChangeEmail}
+                validations={[required, email]}
+              />
+            </div>
+
+            <div className="form-group">
+              <button
+                className="btn btn-primary btn-block"
+                disabled={this.state.loading}
+              >
+                {this.state.loading && (
+                  <span className="spinner-border spinner-border-sm"></span>
+                )}
+                <span>Submit</span>
+              </button>
+            </div>
+
+            <div className="form-group">
+              <label>Need assistance?</label>
+              <Link href="/register" variant="body2">
+                Contact Us
+              </Link>
+            </div>
+            </div>)}
+
+
+            {this.state.message && (
+              <div className="form-group">
+                <div
+                  className={
+                    this.state.successful
+                      ? "alert alert-success"
+                      : "alert alert-danger"
+                  }
+                  role="alert"
+                >
+                  {this.state.message}
+                </div>
+              </div>
+            )}
+            <CheckButton
+              style={{ display: "none" }}
+              ref={(c) => {
+                this.checkBtn = c;
+              }}
+            />
+          </Form>
+        </div>
+      </div>
+    );
+  }
+}
+
+export default ResetPassword;
